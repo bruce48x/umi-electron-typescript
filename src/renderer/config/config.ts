abad074731c37a6d2ae40411bb943a03ea1c36d9
bruce48 <bruce48@foxmail.com>
2020-05-11 09:56:48 +0800

    upgrade umi to v3

--- src/renderer/config/config.ts
+++ src/renderer/config/config.ts
@@ -1,28 +1,10 @@
 const cwd = process.cwd();
 
 export default {
-  history: 'hash',
-  outputPath: `../../dist/renderer`,
-  publicPath: './',
-  plugins: [
-    [
-      'umi-plugin-react',
-      {
-        antd: true,
-        dva: true,
-        dynamicImport: true,
-        dll: true,
-        hardSource: false,
-        routes: {
-          exclude: [/components/],
-        },
-      },
-    ],
-  ],
-  routes: [
-    {
-      path: '/',
-      component: './index',
-    },
-  ],
+    outputPath: `../../dist/renderer`,
+    publicPath: './',
+    antd: {},
+    dva: {},
+    hash: true,
+    // dynamicImport: {},
 };
